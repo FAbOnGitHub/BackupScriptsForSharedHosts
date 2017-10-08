a8f097be87b600741a6d913d7e18388b4ee785da
FAb Mendes <fab@antaya.fr>
2017-10-08 16:36:10 +0200

    Very simple script to backup directories...

--- backup_many_dir.sh
+++ backup_many_dir.sh
@@ -18,61 +18,78 @@ cd $DIR 2>/dev/null; export LIB_PATH=$PWD; cd - >/dev/null
 
 
 if [ ! -f $BAK_DIR/.htaccess ]; then
-    fileLogger "$KO ERR fichier .htaccess inaccessible"
-    rm -f $ARCHIVE_FILE
+    fileLogger "$KO ERR: cannot find .htaccess in $BAK_DIR"
     exit 1
 fi
 if [ -f $LOCK_FILE ]; then
-    fileLogger "$KO ERR verrou '$LOCK_FILE' present... abandon"
+    fileLogger "$KO ERR missing lock file '$LOCK_FILE'... abort"
     exit 1
 fi
 
-if [ "x$ZIP_PASSWD" = "x" ]; then
-    fileLogger "$KO ZIP_PASSWD est vide... abandon"
-    exit 1
-fi
 
 if [ ${#DIR_TO_BACKUP[*]} -lt 1 ]; then
-    fileLogger "$KO pb DIR_TO_BACKUP ... abandon"
+    fileLogger "$KO empty DIR_TO_BACKUP ... abort"
     exit 1
     
 fi    
 
-for d in ${#DIR_TO_BACKUP[*]}
+bTestOnly=0
+if [ "x$1" = "x-t" ]; then
+    bTestOnly=1
+fi
+
+for d in ${DIR_TO_BACKUP[*]}
 do
-    ARCHIVE_FILE=$BAK_DIR/${d}.tgz
-    rm -f $ARCHIVE_FILE
+    taskCount
+    
+    if [ ! -r "$d" ]; then
+        fileLogger "No such file or directory '$d'"
+        taskErr
+        continue
+    fi
+
+    
+    basename_d=$(basename "$d")
+    dirname_d=$(dirname "$d")
+    hash_d=$(echo "$dirname_d"|sha1sum|cut -c-16)
+    file_d="${hash_d}_${basename_d}"
+    
+    ARCHIVE_FILE="$BAK_DIR/${file_d}.tgz"
+
+    if [ $bTestOnly -eq 1 ]; then
+        echo "$d -> $ARCHIVE_FILE in $BAK_DIR"
+        taskOk
+        continue
+    fi
     
 
-
-taskCount
-
-\cd $d
-\cd ..
-tar zcf $ARCHIVE_FILE $d
-if [ $rc -eq 0 ]; then
-    szArch="$(du --si -s $ARCHIVE_FILE | awk '{print $1}')"
-    szDir="$(du --si -s $WIKI_DIR | awk '{print $1}')"
-    fileLogger "$ok $L_DUMP $ARCHIVE_FILE ($szDir->$szArch)"
-    bDoCompress=0
-    do_moveXferZone $ARCHIVE_FILE
+    \cd "$basenane_d"
+    rm -f "$ARCHIVE_FILE"  
+    tar zcf "$ARCHIVE_FILE" "$d"
     rc=$?
-    if [ $rc -eq $EXIT_SUCCESS ]; then
-        taskOk
+    if [ $rc -eq 0 ]; then
+        szArch="$(du --si -s $ARCHIVE_FILE | awk '{print $1}')"
+        szDir="$(du --si -s $d | awk '{print $1}')"
+        fileLogger "$ok $L_DUMP $ARCHIVE_FILE ($szDir->$szArch)"
+        bDoCompress=0
+        do_moveXferZone $ARCHIVE_FILE
+        rc=$?
+        if [ $rc -eq $EXIT_SUCCESS ]; then
+            taskOk
+        else
+            taskErr
+        fi
     else
         taskErr
+        rm -rf $ARCHIVE_FILE
+        fileLogger "$KO $L_DUMP $ARCHIVE_FILE (rc=$rc)"
     fi
-else
-    taskErr
-    rm -rf $ARCHIVE_FILE
-    fileLogger "$KO $L_DUMP $ARCHIVE_FILE (rc=$rc)"
-fi
 
 done
 
 ### Reporting
 taskReportStatus
-sReport="$_taskReportLabel wiki"
+sReport="$_taskReportLabel backup_many_dir"
 logStop "$sReport"
 reportByMail "$sReport" "$ME"
 exit $_iNbTaskErr
