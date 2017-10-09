ccd53f2646d098f50c7e6d6ad41ad7e3b7babc34
FAb Mendes <fab@antaya.fr>
2017-10-09 17:10:33 +0200

    Test to make plugin more easier... with D_ETC variable

--- boot.sh
+++ boot.sh
@@ -64,17 +64,20 @@ msg=$msg"\nloading $LIB_PATH/config_default.sh"
 #  hostname varie... Donc je fixe un nom à config_priv comme
 #  privée
 export D_ETC="$(echo $LIB_PATH | sed -e "s@\/cgi-bin\$@\/cgi-etc@" )"
-if [ ! -d "$D_ETC" ]; then
+if [ "$D_ETC" = "$LIB_PATH" ]; then
+    # So nothing done... must look ahead
     export D_ETC="$(echo "$LIB_PATH/../cgi-etc" )"
-    if [ ! -d "$D_ETC" ]; then
-        die "boot.sh : cannot find D_ETC=\$D_ETC from \$LIB_PATH=$LIB_PATH"
-    fi
-fi
+
+fi   
 if [ "$D_ETC" = "$LIB_PATH" ]; then
     echo "boot.sh :  D_ETC=\$D_ETC and \$LIB_PATH are the same : '$D_ETC'"
     echo "boot.sh :  D_ETC must end with 'cgi-bin'. Fatal error."
     exit 254 # 666 >256
 fi
+if [ ! -d "$D_ETC" ]; then
+    die "boot.sh : cannot find D_ETC=\$D_ETC from \$LIB_PATH=$LIB_PATH"
+fi
+
 
 #  Mais pour les machines des copains on peut encore redéfinir
 #  les variables tranquillou
