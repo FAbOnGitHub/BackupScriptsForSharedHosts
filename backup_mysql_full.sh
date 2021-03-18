553332a6de11da24f663bd813b1b9544356463d5
FAb Mendes <fab@antaya.fr>
2021-03-18 16:28:57 +0100

    fix: remove  double extension

--- backup_mysql_full.sh
+++ backup_mysql_full.sh
@@ -136,13 +136,16 @@ if [ $bDoCompressAll -eq 1 ]; then
     do_compress_clean  "$DIR".zip "$dir"
     rc_x=$?
     # $f_current est à jour
-
-# 6 Move to xfer zone (option)
-    do_moveXferZone "$f_current"
-    rc=$?
-    taskAddAndStatus $rc
-    # rm -rf "$dir" # done by do_moveXferZone
+    bundle="$f_current"
+else
+    bundle="$dir"
 fi
+
+do_moveXferZone "$bundle"
+rc=$?
+taskAddAndStatus $rc
+# rm -rf "$dir" # done by do_moveXferZone
+
 
 mysql_clean_up
 ### Reporting
